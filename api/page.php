19d315ac82399bfa8b44840bd8771f94229d5a7c
wdz <1220555415@qq.com>
2018-07-31 14:50:55 +0800

    ‘添加api

--- api/page.php
+++ api/page.php
@@ -1,24 +1,23 @@
 <?php
-
 include_once("DbUtils.class.php");
 
-$data = array();
 
+// echo "hello";exit();
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $data[':num'] = isset($_GET['num']) ? $_GET['num'] : 10;
+
+$data[':count'] = ($page-1) * $data[':num']; 
+
+// $sql = "SELECT * FROM student LIMIT  0,12 ";
 
 $data[':count'] = ($page-1)*$data[':num']; 
 
 $sql = "SELECT * FROM student LIMIT  $data[':count'],$data[':num'] ";
 
-if (isset($_GET['sort'])){
-	$data[':sort'] = $_GET['sort'];
-	$data[':dir'] = $_GET['dir'];
-	$sql .= "  order by $data[':sort']  $data[':dir']  ";
-}
-
 $db = DbUtils::getDB();
-$outs = $db->get($sql);
+$out = $db->get($sql);
 
-echo json_encode($outs);
+var_dump($out);
+
+
 ?>
