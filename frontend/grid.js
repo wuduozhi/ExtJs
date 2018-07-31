969238e8dc2c8e2be04e87f0b8e868fecb2d2a29
wdz <1220555415@qq.com>
2018-07-31 19:50:52 +0800

    ‘添加grid测试文件

--- frontend/grid.js
+++ frontend/grid.js
@@ -6,15 +6,11 @@ Ext.require([
 Ext.onReady(function(){
     Ext.define('Book',{
         extend: 'Ext.data.Model',
-        fields:[
-            {name:'id',type:'int'},
-            {name:'code',type:'string'},
-            {name:'name',type:'string'},
-            {name:'sex',type:'int'},
-            {name:'age',type:'int'},
-            {name:'political',type:'string'},
-            {name:'origin',type:'string'},
-            {name:'professional',type:'string'}
+        fields: [
+            // set up the fields mapping into the xml doc
+            // The first needs mapping, the others are very basic
+            {name: 'Author', mapping: 'ItemAttributes > Author'},
+            'Title', 'Manufacturer', 'ProductGroup'
         ]
     });
 
@@ -22,38 +18,30 @@ Ext.onReady(function(){
     var store = Ext.create('Ext.data.Store', {
         model: 'Book',
         autoLoad: true,
-        proxy:{
-            type:'ajax',
-            url:'http://wduozhi.xyz/ExtJs/api/page.php',
-            reader:{
-                type:'json',
-                totalProperty:'totalCount',
-                root:'result',
-                idProperty:'id'
+        proxy: {
+            // load using HTTP
+            type: 'ajax',
+            url: 'sheldon.xml',
+            // the return will be XML, so lets set up a reader
+            reader: {
+                type: 'xml',
+                // records will have an "Item" tag
+                record: 'Item',
+                idProperty: 'ASIN',
+                totalRecords: '@total'
             }
-        },
-    });
-    var sexRenderer = function(value){
-        if(value == 1){
-            return '<span style="color:red;font-weight:bold;">男</span>';
-        }else if(value == 2){
-            return '<span style="color:green;font-weight:bold;">女</span>';
         }
-    };
-    var columns = [
-        {header:'学号'，dataIndex:'code'},
-        {header:'姓名'，dataIndex:'name'},
-        {header:'性别'，dataIndex:'sex',renderer:sexRenderer},
-        {header:'年龄'，dataIndex:'age'},
-        {header:'政治面貌'，dataIndex:'political'},
-        {header:'籍贯'，dataIndex:'origin'},
-        {header:'所属系'，dataIndex:'professional'}
-    ];
+    });
 
     // create the grid
     var grid = Ext.create('Ext.grid.Panel', {
         store: store,
-        columns: columns,
+        columns: [
+            {text: "Author", flex: 1, dataIndex: 'Author', sortable: true},
+            {text: "Title", width: 180, dataIndex: 'Title', sortable: true},
+            {text: "Manufacturer", width: 115, dataIndex: 'Manufacturer', sortable: true},
+            {text: "Product Group", width: 100, dataIndex: 'ProductGroup', sortable: true}
+        ],
         renderTo:'example-grid',
         width: 540,
         height: 200
