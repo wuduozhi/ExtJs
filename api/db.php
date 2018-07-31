5d4dcf1ddc24cb0ee0db6ed05524eb6de8b4f3ff
wdz <1220555415@qq.com>
2018-07-31 10:11:57 +0800

    ‘添加api

--- api/db.php
+++ api/db.php
@@ -3,18 +3,18 @@
 $dbhost = 'localhost:3306';  // mysql服务器主机地址
 $dbuser = 'root';            // mysql用户名
 $dbpass = '123456';          // mysql用户名密码
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
 if(! $conn )
 {
-    die('连接失败: ' . mysqli_error($conn));
+    die('连接失败: ' . mysql_error($conn));
 }
 echo '连接成功<br />';
-mysqli_select_db( $conn, 'student' );
+mysql_select_db( $conn, 'student' );
 // $retval = mysqli_query( $conn, $sql );
 // if(! $retval )
 // {
 //     die('数据表创建失败: ' . mysqli_error($conn));
 // }
 echo "数据表创建成功\n";
-mysqli_close($conn);
+mysql_close($conn);
 ?>
