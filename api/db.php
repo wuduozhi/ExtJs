d955fa101d6c9bbfc86a4cb9eb19d8f76ea2c750
wdz <1220555415@qq.com>
2018-07-31 10:58:06 +0800

    ‘添加api

--- api/db.php
+++ api/db.php
@@ -1,20 +1,14 @@
-
 <?php
-$dbhost = 'localhost:3306';  // mysql服务器主机地址
-$dbuser = 'root';            // mysql用户名
-$dbpass = 'root';          // mysql用户名密码
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-if(! $conn )
-{
-    die('连接失败: ' . mysql_error($conn));
-}
-echo '连接成功<br />';
-mysql_select_db( $conn, 'student' );
-// $retval = mysqli_query( $conn, $sql );
-// if(! $retval )
-// {
-//     die('数据表创建失败: ' . mysqli_error($conn));
-// }
-echo "数据表创建成功\n";
-mysql_close($conn);
+require_once("DbUtils.class.php");
+
+
+$db = DbUtils::getDB();
+
+$sql = "SELECT * FROM studnet";
+
+$out = $db->gets($sql);
+
+var_dump($out);
+
+
 ?>
